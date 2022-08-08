da57676c17bcb5ed732626ee78d54abaf1f18eb6
DJ Bates <blooviper@gmail.com>
2022-08-08 14:05:31 -0600

    changes made

--- CSharpMethodsEX1Console/Program.cs
+++ CSharpMethodsEX1Console/Program.cs
@@ -19,10 +19,10 @@
             Console.WriteLine("Awesome!, Okay last question: What is your favorite band?");
             var favoriteBand = Console.ReadLine();
 
-            Console.WriteLine($"Okay {userName}, here is a story that I made up using your responses:");
-            Console.WriteLine($"One summer evening there was a {favoriteAnimal} named {userName}.");
-            Console.WriteLine($"{userName} was a curious little guy and wanted to know how high he could fly");
-            Console.WriteLine($"So he turned on some '{favoriteBand}', hopped in a {favoriteColor} rocket, and he took to the sky");
+            Console.WriteLine($"Okay {userName}, here is a story that I made up using your responses:\n" +
+                              $"One summer evening there was a {favoriteAnimal} named {userName}.\n" +
+                              $"{userName} was a curious little guy and wanted to know how high he could fly\n" +
+                              $"So he turned on some '{favoriteBand}', hopped in a {favoriteColor} rocket, and he took to the sky");
 
           
         }
